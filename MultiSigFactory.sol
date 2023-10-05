43584a508483ecb349c9366b937db26861223f3c
Paul Elisha <ireajao@yahoo.com>
2023-10-05 01:25:58 +0100

    first commit

---
+++ MultiSigFactory.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.12;
+
+import {MultiSig} from "./multiSig.sol";
+
+contract MultiSigFactory {
+    MultiSig[] public _multisig;
+    event Create(MultiSig _adr);
+
+    function createMultisig(address[] memory _admins) external payable returns (MultiSig newMultisig) {
+        newMultisig = new MultiSig{value: msg.value}(_admins);
+        _multisig.push(newMultisig);
+        emit Create(newMultisig);
+    }
+
+    function getAllMultisig() external view returns (MultiSig[] memory) {
+        return _multisig;
+    }
+}
